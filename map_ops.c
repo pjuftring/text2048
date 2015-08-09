6a6fcea24b0d70e740fe248a34297c36f03b09eb
Christian Fiedler <e1363562@student.tuwien.ac.at>
2015-08-09 23:38:32 +0200

    initial commit with current codebase

---
+++ map_ops.c
@@ -0,0 +1,102 @@
+
+/*
+void extract_map(unsigned int array[GAME_COLS][GAME_COLS]){
+	int i;
+
+	for(i = 0; i < GAME_COLS; i++){
+		array[0][i] = map[i] & 0xFF;
+		array[1][i] = (map[i] >> 8) & 0xFF;
+		array[2][i] = (map[i] >> 16) & 0xFF;
+		array[3][i] = (map[i] >> 24) & 0xFF;
+	}
+}
+
+void compress_map(const unsigned int array[GAME_COLS][GAME_COLS]){
+	int i;
+
+	for(i = 0; i < GAME_COLS; i++){
+		map[i] = array[0][i] | (array[1][i] << 8) | 
+			(array[2][i] << 16) | (array[3][i] << 24);
+	}
+}
+*/
+
+int toleft(int line){
+	int i = 0, j = 0, actions = 0;
+	for(i = 0; i < GAME_COLS; i++){
+		if(map[i][line] != 0){
+			if(i != j){
+				map[j][line] = map[i][line];
+				map[i][line] = 0;
+				actions++;
+			}
+			j++;
+		}
+	}
+	return actions;
+}
+
+int map_leftop(void){
+	int i,line;
+	int actions = 0;
+
+	for(line = 0; line < GAME_COLS; line++){
+		actions += toleft(line);
+		for(i = 0; i < GAME_COLS-1; i++){
+			if(map[i][line] != 0 && map[i][line] == map[i+1][line]){
+				map[i][line]++;
+				map[i+1][line] = 0;
+				actions++;
+			}
+		}
+		actions += toleft(line);
+	}
+
+	return actions;
+}
+
+void map_transpose(void){
+	int x,y;
+	for(y = 0; y < GAME_COLS; y++){
+		for(x = y+1; x < GAME_COLS; x++){
+			swap_int(&map[x][y], &map[y][x]);
+		}
+	}
+}
+
+void map_rotate_left(int times){
+	int x,y;
+
+	times %= 4;
+	if(times == 0) return;
+
+	map_transpose();
+	for(x = 0; x < GAME_COLS; x++){
+		for(y = 0; y < GAME_COLS/2; y++){
+			swap_int(&map[x][y], &map[x][GAME_COLS-y-1]);
+		}
+	}
+
+	map_rotate_left(times-1);	
+}
+
+int map_upop(void){
+	map_rotate_left(1);
+	int actions = map_leftop();
+	map_rotate_left(3);
+	return actions;
+}
+
+int map_downop(void){
+	map_rotate_left(3);
+	int actions = map_leftop();
+	map_rotate_left(1);
+	return actions;
+}
+
+int map_rightop(void){
+	map_rotate_left(2);
+	int actions = map_leftop();
+	map_rotate_left(2);
+	return actions;
+}
---
+++ text2048.c
@@ -0,0 +1,160 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <curses.h>
+
+#define GAME_COLS	4
+
+void swap_int(int *const a, int *const b){
+	if(a != b){
+		register int tmp = *a;
+		*a = *b;
+		*b = tmp;
+	}
+}
+
+unsigned int map[GAME_COLS][GAME_COLS] = {0}; 
+
+#include "map_ops.c"
+
+int pow2(int exp)
+{
+	int x = 1;
+	if(exp < 0) return 0;
+	while(exp--)
+		x *= 2;
+	return x;
+}
+
+void draw(void)
+{
+	int x,y;
+	char str[50];
+
+	clear();
+
+	for(y = 0; y < GAME_COLS; y++){
+		for(x = 0; x < GAME_COLS; x++){
+			const int text_y = 2*y+2, text_x = 8*x+2;
+
+			if(map[x][y] == 0){
+				color_set(2+map[x][y]/2, 0);
+				mvaddstr(text_y, text_x, "       ");
+				
+			}else{
+				if(map[x][y] % 2 == 1){
+					color_set(1, 0);
+					mvaddstr(text_y, text_x, "       ");
+					sprintf(str, "%5d", pow2(map[x][y]));
+					color_set(2+map[x][y]/2, 0);
+					mvaddstr(text_y, text_x+1, str);
+				}else{
+					sprintf(str, " %5d ", pow2(map[x][y]));
+					color_set(2+map[x][y]/2, 0);
+					mvaddstr(text_y, text_x, str);
+				}
+			}
+		}
+	}
+
+	refresh();
+}
+
+void shuffle(int vec[], const int len)
+{
+	int i,pos1,pos2;
+	for(i = 0; i < 2*len; i++){
+		pos1 = rand() % GAME_COLS;
+		pos2 = rand() % GAME_COLS;
+		
+		swap_int(&vec[pos1], &vec[pos2]);
+	}
+
+}
+
+int spawn_number(void)
+{
+	int line_vec[GAME_COLS], col_vec[GAME_COLS];
+	int i,j;
+	
+	for(i = 0; i < GAME_COLS; i++)
+		line_vec[i] = i;
+	for(i = 0; i < GAME_COLS; i++)
+		col_vec[i] = i;
+
+	shuffle(col_vec,GAME_COLS);
+	shuffle(line_vec,GAME_COLS);
+
+	for(j = 0; j < GAME_COLS; j++){
+		for(i = 0; i < GAME_COLS; i++){
+			const int x = col_vec[i], y = line_vec[j];
+			
+			if(map[x][y] == 0){
+				int w = (rand() % 40)/4;
+				if(w == 0)
+					map[x][y] = 2;
+				else
+					map[x][y] = 1;
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	initscr();
+	atexit((void (*)(void)) endwin);
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_RED); 
+	init_pair(2, COLOR_WHITE, COLOR_YELLOW); 
+	init_pair(3, COLOR_WHITE, COLOR_GREEN); 
+	init_pair(4, COLOR_WHITE, COLOR_BLUE); 
+	init_pair(5, COLOR_WHITE, COLOR_MAGENTA);
+	init_pair(6, COLOR_YELLOW, COLOR_CYAN);
+	init_pair(7, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(8, COLOR_GREEN, COLOR_BLACK);
+	init_pair(9, COLOR_RED, COLOR_BLACK);
+	init_pair(10, COLOR_WHITE, COLOR_BLACK);
+	attron(A_BOLD);
+	curs_set(0);
+	cbreak();
+	keypad(stdscr, TRUE);
+
+	spawn_number();
+
+	int key;
+	for(;;){
+		draw();
+
+		key = getch();
+		if(tolower(key) == 'q') break;
+		
+
+		switch(key){
+		case KEY_LEFT:
+			if(map_leftop())
+				spawn_number();
+			break;
+		case KEY_RIGHT:
+			if(map_rightop())
+				spawn_number();
+			break;
+		case KEY_UP:
+			if(map_upop())
+				spawn_number();
+			break;
+		case KEY_DOWN:
+			if(map_downop())
+				spawn_number();
+			break;
+		}
+	}
+
+	return 0;
+}
